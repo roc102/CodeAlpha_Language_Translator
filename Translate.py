0f3f87cd69cdbdd1717d916623323c9ef487e9b9
roc102 <kyroc102@gmail.com>
2023-08-02 23:27:25 +0530

    updated translator

--- Translate.py
+++ Translate.py
@@ -1,23 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
-from googletrans import Translator
+from googletrans import Translator, LANGUAGES
 
 def translate_text():
     text = text_entry.get()
-    translation = translate(text)
+    source_lang = source_lang_var.get()
+    dest_lang = dest_lang_var.get()
+    translation = translate(text, source_lang, dest_lang)
     translation_label.config(text=translation)
 
-def translate(text):
+def translate(text, source_lang, dest_lang):
     translator = Translator()
-    translated = translator.translate(text, dest='en')
+    translated = translator.translate(text, src=source_lang, dest=dest_lang)
     return translated.text
 
 def main():
-    global text_entry, translation_label  # Declare text_entry and translation_label as global variables
+    global text_entry, translation_label, source_lang_var, dest_lang_var  # Declare variables as global
 
     root = tk.Tk()
     root.title("Language Translator")
-    root.geometry("400x300")
+    root.geometry("500x350")
 
     style = ttk.Style()
     style.configure("TLabel", font=("Arial", 12))
@@ -26,16 +28,32 @@ def main():
     title_label = ttk.Label(root, text="Language Translator", font=("Arial", 16, "bold"), padding=(0, 10))
     title_label.pack()
 
+    source_lang_label = ttk.Label(root, text="Source Language:")
+    source_lang_label.pack(pady=5)
+
+    source_lang_var = tk.StringVar()
+    source_lang_combobox = ttk.Combobox(root, textvariable=source_lang_var, values=list(LANGUAGES.values()))
+    source_lang_combobox.pack(pady=5)
+    source_lang_combobox.set("English")
+
     label = ttk.Label(root, text="Enter text:")
     label.pack(pady=5)
 
     text_entry = ttk.Entry(root, font=("Arial", 12), width=40)
     text_entry.pack(pady=5)
 
+    dest_lang_label = ttk.Label(root, text="Destination Language:")
+    dest_lang_label.pack(pady=5)
+
+    dest_lang_var = tk.StringVar()
+    dest_lang_combobox = ttk.Combobox(root, textvariable=dest_lang_var, values=list(LANGUAGES.values()))
+    dest_lang_combobox.pack(pady=5)
+    dest_lang_combobox.set("Spanish")
+
     translate_button = ttk.Button(root, text="Translate", command=translate_text)
     translate_button.pack(pady=10)
 
-    translation_label = ttk.Label(root, text="", font=("Arial", 12), wraplength=350, justify="center")
+    translation_label = ttk.Label(root, text="", font=("Arial", 12), wraplength=450, justify="center")
     translation_label.pack()
 
     footer_label = ttk.Label(root, text="Made By Fahad", font=("Arial", 10, "italic"), padding=(0, 10))
